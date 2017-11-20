4c93d963adb8694caddcd85a0365e36ed8166056
rahlk <i.m.ralk@gmail.com>
2017-11-20 13:29:11 -0500

    Autocommit

--- bug_miner/Utils/MetricsUtil.py
+++ bug_miner/Utils/MetricsUtil.py
@@ -1,77 +1,61 @@
-from __future__ import division
 from __future__ import print_function
-
 import os
-import subprocess
+import sys
+import shlex
+import pandas as pd
 from pdb import set_trace
+import warnings
+from datetime import datetime
+import dateutil
+import subprocess
+from glob2 import glob
 
-from FileUtils import XMLUtil
+warnings.filterwarnings("ignore")
 
-root = os.getcwd()
-
+root = os.path.join(os.getcwd().split("bug_miner")[0], "bug_miner")
+if root not in sys.path:
+    sys.path.append(root)
 
 class JavaUtil:
-    def __init__(self, tags, project_path, jar_path):
+    def __init__(self, tags, project_path, jar_path, save_path):
         self.tags = tags
         self.jar_path = jar_path
+        self.save_path = save_path
         self.project_path = project_path
+        self.version_commits = version_commits
+        self.ckjm_path = os.path.expanduser(
+            "~/git/ckjm/target/runable-ckjm_ext-2.3-SNAPSHOT.jar")
 
     @staticmethod
-    def _run_ckjm(jar):
-        cmd = ["java", "-jar", os.path.join(root, "tools/ckjm.jar"), 
-                "-x", "-s", jar]
-        return subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=open(os.devnull, "w"))
-
-    @staticmethod
-    def _run_maven():
-        cmd = ["sh", "build.sh"]
-        return subprocess.Popen(cmd)
+    def _run_ckjm():
+       for class_file in glob(project_path + "/**/*.class"):
+        cmd = shlex.split("java -jar {} -s {}".format(
+            self.ckjm_path, class_file))
+        yield subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
     
     @staticmethod
     def _git_checkout(commit_hash):
-        cmd = ["git", "checkout", commit_hash]
-        return subprocess.Popen(cmd)
-
-    
-    def _copy_to_jars(self, name):
-        cmd = ["cp", self.project_path+"/build/lib/ant.jar", self.jar_path+"ant-{}".format(name)]
-        return subprocess.Popen(cmd)
+        cmd = "git --git-dir {} checkout {}".format(self.project_path, commit_hash)
+        return subprocess.call(cmd, shell=True)
 
     def save_metrics(self):
 
         os.chdir(self.project_path) # Change directory to project
-        for tag, commit_hash in self.tags.iteritems():
-            metrics = []
-            
-            print("+ Apache Ant {}".format(tag))
+        for version in self.version_commits:
+            v_name = version.split("/")[-1].split('.')[0]
+            set_trace()
+            hashes = pd.read_csv(version)["Hash"]
             print("\t+ -- Checkout commit")
-            self._git_checkout(commit_hash)
-            print("\t+ -- Build project")
-            self._run_maven()
-            print("\t+ -- Copy compiled jar")
-            self._copy_to_jars(name=tag)
-
-            # set_trace()
-
-        for jar in jarfiles:
-            metrics.append(self._run_ckjm(jar).communicate()[0])
-
-        print("<metrics>", "\n".join(metrics), "</metrics>", sep="\n",
-                file=open(os.path.join(self.save_path, version + ".xml"), "w+"))
+            self._git_checkout(hashes.value[0])                
+            print("\t+ -- Compute static code metrics")
+            metrics = [out for out in self._run_ckmj()]
+            print("<metrics>", "\n".join(metrics), "</metrics>", sep="\n",
+                    file=open(os.path.join(self.save_path, v_name + ".xml"), "w+"))
 
 
-def __test_util():
-    """
-    Run a test case
-    :return:
-    """
-    m = JavaUtil(jar_file="data/ant-1.8.2/build/lib/ant.jar",
-                 file_name="ant.xml")
-    m.save_metrics()
-    xml = XMLUtil(metrics_name="ant.xml")
-    xml.save_as_csv()
-
+def __test_metrics():
+    pass
 
 if __name__ == "__main__":
-    __test_util()
-    set_trace()
+    __test_metrics()
