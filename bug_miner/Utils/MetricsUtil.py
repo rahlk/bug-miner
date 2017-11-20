bc3e19bdd2b7b9b597375f83f15b6a417331dc82
rahlk <i.m.ralk@gmail.com>
2017-11-20 16:24:02 -0500

    ii

--- bug_miner/Utils/MetricsUtil.py
+++ bug_miner/Utils/MetricsUtil.py
@@ -17,45 +17,54 @@ if root not in sys.path:
     sys.path.append(root)
 
 class JavaUtil:
-    def __init__(self, tags, project_path, jar_path, save_path):
-        self.tags = tags
-        self.jar_path = jar_path
+    def __init__(self, project_path, version_commits, save_path):
         self.save_path = save_path
         self.project_path = project_path
         self.version_commits = version_commits
         self.ckjm_path = os.path.expanduser(
-            "~/git/ckjm/target/runable-ckjm_ext-2.3-SNAPSHOT.jar")
+            "~/ckjm/target/runable-ckjm_ext-2.3-SNAPSHOT.jar")
 
-    @staticmethod
-    def _run_ckjm():
-       for class_file in glob(project_path + "/**/*.class"):
-        cmd = shlex.split("java -jar {} -s {}".format(
-            self.ckjm_path, class_file))
-        yield subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+    def _run_ckjm(self, jar_file):
+        cmd = shlex.split("java -jar {} -s -x {}".format(self.ckjm_path, jar_file))
+        set_trace()
+        return subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
     
-    @staticmethod
-    def _git_checkout(commit_hash):
-        cmd = "git --git-dir {} checkout {}".format(self.project_path, commit_hash)
-        return subprocess.call(cmd, shell=True)
+    def _git_checkout(self, commit_hash):
+        cmd = shlex.split(
+            "git -C {} checkout {}".format(self.project_path, commit_hash))
+        return subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+
+    def _build(self):
+        pwd = os.getcwd()
+        os.chdir(self.project_path)
+        cmd = ["sh", "build.sh"]
+        call = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        os.chdir(pwd)
+        return call
 
     def save_metrics(self):
-
-        os.chdir(self.project_path) # Change directory to project
         for version in self.version_commits:
-            v_name = version.split("/")[-1].split('.')[0]
-            set_trace()
-            hashes = pd.read_csv(version)["Hash"]
-            print("\t+ -- Checkout commit")
-            self._git_checkout(hashes.value[0])                
+            v_name = version.split("/")[-1].split('.jar')[0]
+            # hashes = pd.read_csv(version)["Hash"]
+            # print("\t+ -- Checkout commit")
+            # checkout_status = self._git_checkout(hashes[0])
+            # print("\t+ -- Build commits")
+            # build_status = self._build()
+            # set_trace()
             print("\t+ -- Compute static code metrics")
-            metrics = [out for out in self._run_ckmj()]
+            metrics = self._run_ckjm(version)
             print("<metrics>", "\n".join(metrics), "</metrics>", sep="\n",
                     file=open(os.path.join(self.save_path, v_name + ".xml"), "w+"))
 
 
 def __test_metrics():
-    pass
+    version_commits = glob(os.path.abspath(root+"/bug_miner/projects/jars/*.jar"))
+    project_path = os.path.expanduser("~/git/mining/ant/")
+    save_path = os.path.join(root, "bug_miner/data/ant")
+    j_util = JavaUtil(project_path, version_commits, save_path)
+    j_util.save_metrics()
+    set_trace()
 
 if __name__ == "__main__":
     __test_metrics()
