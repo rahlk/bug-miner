4674076b6402a206b95a026fbc7e3e9be2fa50f6
rahlk <i.m.ralk@gmail.com>
2017-11-20 17:00:36 -0500

    i

--- bug_miner/Utils/MetricsUtil.py
+++ bug_miner/Utils/MetricsUtil.py
@@ -22,12 +22,14 @@ class JavaUtil:
         self.project_path = project_path
         self.version_commits = version_commits
         self.ckjm_path = os.path.expanduser(
-            "~/ckjm/target/runable-ckjm_ext-2.3-SNAPSHOT.jar")
+            "~/git/ckjm/target/runable-ckjm_ext-2.3-SNAPSHOT.jar")
 
-    def _run_ckjm(self, jar_file):
-        cmd = shlex.split("java -jar {} -s -x {}".format(self.ckjm_path, jar_file))
+    def _run_ckjm(self):
         set_trace()
-        return subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+        for class_file in glob(os.path.join(self.project_path, "**/*.class")):
+            cmd = shlex.split("java -jar {} -s -x {}".format(self.ckjm_path, class_file))
+            set_trace()
+            yield subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[1]
     
     def _git_checkout(self, commit_hash):
         cmd = shlex.split(
@@ -45,21 +47,21 @@ class JavaUtil:
 
     def save_metrics(self):
         for version in self.version_commits:
-            v_name = version.split("/")[-1].split('.jar')[0]
-            # hashes = pd.read_csv(version)["Hash"]
-            # print("\t+ -- Checkout commit")
-            # checkout_status = self._git_checkout(hashes[0])
-            # print("\t+ -- Build commits")
-            # build_status = self._build()
-            # set_trace()
+            v_name = version.split("/")[-1].split('.')[0]
+            hashes = pd.read_csv(version)["Hash"]
+            print("\t+ -- Checkout commit")
+            checkout_status = self._git_checkout(hashes[0])
+            print("\t+ -- Build commits")
+            build_status = self._build()
+            set_trace()
             print("\t+ -- Compute static code metrics")
-            metrics = self._run_ckjm(version)
+            metrics = [out for out in self._run_ckjm()]
             print("<metrics>", "\n".join(metrics), "</metrics>", sep="\n",
                     file=open(os.path.join(self.save_path, v_name + ".xml"), "w+"))
 
 
 def __test_metrics():
-    version_commits = glob(os.path.abspath(root+"/bug_miner/projects/jars/*.jar"))
+    version_commits = glob(os.path.abspath(root+"/bug_miner/projects/raw/ant/*.csv"))
     project_path = os.path.expanduser("~/git/mining/ant/")
     save_path = os.path.join(root, "bug_miner/data/ant")
     j_util = JavaUtil(project_path, version_commits, save_path)
